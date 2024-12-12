67a5c78b761b448f1ada622bcc12fff42a6a0ba5
Connor Delaney <157986156+Sconman@users.noreply.github.com>
2024-12-12 10:25:23 -0500

    Update Death First Search.py
    Removed unnecessary debugging print statements

--- Codingame/Death First Search.py
+++ Codingame/Death First Search.py
@@ -9,11 +9,11 @@ import math
 # e: the number of exit gateways
 network = {}
 n, l, e = [int(i) for i in input().split()]
-print(f"n: {n}, l: {l},e: {e}", file=sys.stderr, flush=True)
+
 for i in range(l):
     # n1: N1 and N2 defines a link between these nodes
     n1, n2 = [int(j) for j in input().split()]
-    #print(f"n1: {n1}, n2: {n2}", file=sys.stderr, flush=True)
+   
     if n1 not in network:
         network[n1] = set()
     if n2 not in network:
@@ -24,25 +24,24 @@ for i in range(l):
 gateways = set()
 for i in range(e):
     ei = int(input())  # the index of a gateway node
-    print(f"gatewayindex: {ei}", file=sys.stderr, flush=True)
+  
     gateways.add(ei)
 
 
-print("Network adjacency list:", file=sys.stderr, flush=True)
-for key, value in network.items():
-    print(f"  {key}: {sorted(value)}", file=sys.stderr, flush=True)
+
+   
 # game loop
 while True:
     si = int(input())  # The index of the node on which the Bobnet agent is positioned this turn
     l1 = None
     l2 = None
-    print(f"bobnetindex: {si}", file=sys.stderr, flush=True)
+    
     for gateway in gateways:
         if gateway in network[si]:
             l1 = si
             l2 = gateway
             #print(si,gateway)
-            link_choice = True
+            
             
     if l1 is None:
         for gateway in gateways:
@@ -63,4 +62,4 @@ while True:
 
 
     # Example: 0 1 are the indices of the nodes you wish to sever the link between
-    #print(si, ei)
+    
